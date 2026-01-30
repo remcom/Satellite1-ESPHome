0f9b56c9c0cf1426174ea8c9078a11294e3c5b53
Remco van Essen <remco.essen@gmail.com>
2026-01-30 19:25:31 +0100

    Reapply "rework fusb302b"
    This reverts commit 8a660e678dcaef1faf14257b9728a06eea439451.

--- esphome/components/fusb302b/pd.h
+++ esphome/components/fusb302b/pd.h
@@ -293,11 +293,9 @@ inline bool PowerDelivery::handle_cntrl_message_(const PDMsg &msg){
     default:
       this->send_message_(PDMsg(pd_control_msg_type::PD_CNTRL_NOT_SUPPORTED));
       break;
-     break;  
   }
   return true;
 }
 
-
-}
-}
+}  // namespace power_delivery
+}  // namespace esphome
